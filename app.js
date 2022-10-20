5a556c055a2950b6343a437de2fd3ebd5ac2302a
cris664 <cristianliu.1999@gmail.com>
2022-10-19 21:32:00 -0300

    port

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 const express = require("express")
 const app = express()
 const path = require("path")
-const port = process.env.PORT || 3001
 app.use(express.static("public"))
-app.listen(port,()=>console.log('Servidor corriendo en el puerto ${port}'))
+app.listen( process.env.PORT || 3000,()=>console.log('Servidor corriendo en el puerto 3000' ))
 app.get("/",(req,res)=>{
     res.sendFile(path.join(__dirname,"./views/home.html"))
 })
